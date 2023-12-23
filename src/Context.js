560871e77ac03aa3ceda949f30951b5348145c51
bbsgms <154452836+bbsgms@users.noreply.github.com>
2023-12-23 09:04:14 +0600

    Update Context.js

--- src/Context.js
+++ src/Context.js
@@ -6,15 +6,27 @@ const DomikContext = React.createContext();
     domiki:[],
     sortedDomiki:[],
     homeDomiki:[],
-    loading: true
+    loading: true,
+    type: 'все',
+    price: 0,
+    minPrice: 0,
+    maxPrice: 0,
+    minSize: 0,
+    maxSize: 0,
+    breakfast: false
   };
   
 componentDidMount(){
   
   let domiki = this.formatData(items)
   let  homeDomiki = domiki.filter( domik => domik.homed === true );
+  let maxPrice = Math.max(...domiki.map(item => item.price));
+  let maxSize = Math.max(...domiki.map(item => item.size));
+
   this.setState({
-    domiki,homeDomiki, sortedDomiki:domiki, loading:false
+    domiki,homeDomiki, sortedDomiki:domiki, loading:false, price: maxPrice,
+    maxPrice,
+    maxSize
   })
 }
 
@@ -35,10 +47,54 @@ getDomik = (info) =>{
   return domik;
 }
 
+handleChange = event=>{
+  const target = event.target;
+  const value = target.type === "checkbox" ? target.checked : target.value;
+  const name = target.name;
+  console.log(name, value);
 
+
+  this.setState(
+      {
+        [name]: value
+      },
+      this.filterDomiki
+    );
+  };
+  filterDomiki = () => {
+    let {
+      domiki,
+      type,
+      price,
+      minSize,
+      maxSize,
+      breakfast
+    } = this.state;
+
+    let tempDomiki = [...domiki];
+    price = parseInt(price);
+    // filter by type
+    if (type !== "все") {
+      tempDomiki = tempDomiki.filter(domik => domik.type === type);
+    }
+    // filter by price
+    tempDomiki = tempDomiki.filter(domik => domik.price <= price);
+    //filter by size
+    tempDomiki = tempDomiki.filter(
+      domik => domik.size >= minSize && domik.size <= maxSize
+    );
+    //filter by breakfast
+    if (breakfast) {
+      tempDomiki = tempDomiki.filter(domik => domik.breakfast === true);
+    }
+    
+    this.setState({
+      sortedDomiki: tempDomiki
+    });
+}
   render() {
     return (
-      <DomikContext.Provider value = {{...this.state, getDomik: this.getDomik}}>
+      <DomikContext.Provider value = {{...this.state, getDomik: this.getDomik, handleChange: this.handleChange}}>
         {this.props.children}
 
       </DomikContext.Provider>
@@ -47,6 +103,7 @@ getDomik = (info) =>{
   }
 }
 const DomikConsumer = DomikContext.Consumer;
+export{DomikProvider, DomikConsumer, DomikContext};
 
 export function withDomikConsumer(Component){
   return function ConsumerWrapper(props){
@@ -56,4 +113,3 @@ export function withDomikConsumer(Component){
   }
 }
 
-export {DomikProvider, DomikConsumer, DomikContext};
